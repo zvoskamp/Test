da83dfaf364f7063424e4993146a01679c42567a
zvoskamp <33737054+zvoskamp@users.noreply.github.com>
2017-11-19 13:17:53 -0500

    update script

--- R/read_db_write_csv.R
+++ R/read_db_write_csv.R
@@ -2,28 +2,37 @@
 #Load Packages
 install.packages('RODBC')
 install.packages('data.table')
+install.packages('dplyr')
+library(dplyr)
 library(RODBC)
 library(data.table)
 
-setwd('Path where to save')
+setwd('E:\\csv')
 
-con <- RODBC::odbcConnect(dsn = <>, uid = '', pwd = '')
+con <- RODBC::odbcConnect(dsn = 'SQL')
+
+databases <- sqlQuery(con, "SELECT name FROM master..sysdatabases where name like '%IA%'")
 
 #List the tables
-tables <- as.list(sqlTables(con))
+tables <- sqlTables(con, catalog = "IA_DB") 
+
+# %>%
+#   SELECT(TABLE_NAME)
+
+tables <- as.list(tables$TABLE_NAME)
 
 #Function to read and write the tables as csv
 readTables <- function(table, dbConnection)
 {
   #Query to select from tables
-  query <- paste0('SELECT * FROM ', table)
+  query <- paste0('SELECT * FROM IA_DB.dbo.', table)
   
   #select * from tables and store as df
-  df <- RODBC::sqlQuery(dbConnection, query)  
+  df <- as.data.frame(RODBC::sqlQuery(dbConnection, query))
   
   #Write tables as csv
-  fwrite(df, paste0(table, '.csv'), sep = '|')
+  fwrite(df, paste0('E:/csv/', table, '.csv'), sep = '|')
 }
 
 #Actually execute the function readTables
-lapply(tables, readTables,  con)
+lapply(tables, readTables, dbConnection = con)
